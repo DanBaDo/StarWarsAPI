78de57168a5b485e02bd593c359b8d1562ed24bf
Daniel Banore Dopico <daniel.banobre.dopico@pm.me>
2022-01-15 07:33:09 +0100

    Fix controllers imports

--- src/api/controllers/__init__.py
+++
@@ -1,3 +0,0 @@
-from api.controllers.characters_controllers import add_character, get_all_characters, get_character_by_id
-from api.controllers.planets_controllers import add_planet, get_all_planets, get_planet_by_id
-from api.controllers.users_controllers import add_user, get_all_users, get_user_by_id
--- src/api/endpoints/endpoints_planet.py
+++ src/api/endpoints/endpoints_planet.py
@@ -1,5 +1,5 @@
 from api import app
-from api.controllers import add_planet, get_all_planets, get_planet_by_id
+from api.controllers.planets_controllers import *
 
 app.add_url_rule("/planets/", methods=["GET"], view_func=get_all_planets)
 app.add_url_rule("/planets/<int:planet_id>/", methods=["GET"], view_func=get_planet_by_id)
